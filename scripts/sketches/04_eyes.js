359f5a55ca9e8f0496d0f97f10758cc5631299ec
Jahyun Oh <soharu1024@gmail.com>
2019-05-22 23:34:56 +0900

    Extract Eye class from draw()

--- scripts/sketches/04_eyes.js
+++ scripts/sketches/04_eyes.js
@@ -1,66 +1,66 @@
+class Eye {
+  constructor(radius) {
+    this.outerRadius = radius;
+    this.innerRadius = radius / 2;
+  }
+
+  draw(p, x, y) {
+    p.push();
+    p.stroke('black');
+
+    // eyeball
+    p.fill('white');
+    p.circle(x, y, this.outerRadius * 2);
+
+    // pupil
+    p.fill('black');
+    // https://ko.wikipedia.org/wiki/%EC%82%BC%EA%B0%81%ED%95%A8%EC%88%98
+    const dx = (p.width / 2) - p.mouseX;
+    const dy = (p.height / 2) - p.mouseY;
+    const h = Math.sqrt(dx * dx + dy * dy);
+    const sinTheta = dy / h;
+    const cosTheta = dx / h;
+    const pupil = {
+      x: x - (this.outerRadius - this.innerRadius) * cosTheta,
+      y: y - (this.outerRadius - this.innerRadius) * sinTheta
+    };
+    p.circle(pupil.x, pupil.y, this.innerRadius * 2);
+    p.pop();
+  }
+}
+
 const eyes = (p) => {
+  const face = {
+    x: Config.sketch.width / 2,
+    y: Config.sketch.height / 2,
+    r: 120
+  };
+  const eyeRadius = 40;
+  const leftEye = new Eye(eyeRadius);
+  const rightEye = new Eye(eyeRadius);
+  const leftCenter = {
+    x: face.x - (face.r - eyeRadius) * Math.cos(Math.PI / 4),
+    y: face.y - (face.r - eyeRadius) * Math.sin(Math.PI / 4),
+  };
+
+  const rightCenter = {
+    x: face.x + (face.r - eyeRadius) * Math.cos(Math.PI / 4),
+    y: face.y - (face.r - eyeRadius) * Math.sin(Math.PI / 4),
+  };
+
   p.setup = () => {
     p.createCanvas(Config.sketch.width, Config.sketch.height);
   };
 
   p.draw = () => {
-    const eyeballR = 40;
-
-    const face = {
-      x: Config.sketch.width / 2,
-      y: Config.sketch.height / 2,
-      r: 120
-    };
-
-    const leftCenter = {
-      x: face.x - (face.r - eyeballR) * Math.cos(Math.PI / 4),
-      y: face.y - (face.r - eyeballR) * Math.sin(Math.PI / 4),
-    };
-
-    const rightCenter = {
-      x: face.x + (face.r - eyeballR) * Math.cos(Math.PI / 4),
-      y: face.y - (face.r - eyeballR) * Math.sin(Math.PI / 4),
-    };
-
     p.background(Palette.lightGray);
 
     p.stroke('black');
     p.fill('yellow');
     p.circle(face.x, face.y, face.r * 2);
 
-    // eyeballs
-    p.fill('white');
-    p.circle(leftCenter.x, leftCenter.y, eyeballR * 2);
-    p.circle(rightCenter.x, rightCenter.y, eyeballR * 2);
-
-    // pupils
-    // https://ko.wikipedia.org/wiki/%EC%82%BC%EA%B0%81%ED%95%A8%EC%88%98
-    const dx = face.x - p.mouseX;
-    const dy = face.y - p.mouseY;
-    const h = Math.sqrt(dx * dx + dy * dy);
-    const sinTheta = dy / h;
-    const cosTheta = dx / h;
-    const pupilR = 20;
-    const pupils = {
-      left: {
-        x: leftCenter.x - (eyeballR - pupilR) * cosTheta,
-        y: leftCenter.y - (eyeballR - pupilR) * sinTheta,
-      },
-      right: {
-        x: rightCenter.x - (eyeballR - pupilR) * cosTheta,
-        y: rightCenter.y - (eyeballR - pupilR) * sinTheta,
-      }
-    };
-    p.fill('#000');
-    p.circle(pupils.left.x, pupils.left.y, eyeballR);
-    p.circle(pupils.right.x, pupils.right.y, eyeballR);
-
-    // for debug
-    p.noFill();
-    p.stroke(255, 255, 255, 128);
-    p.circle(leftCenter.x, leftCenter.y, eyeballR);
-    p.circle(rightCenter.x, rightCenter.y, eyeballR);
-    p.circle(face.x, face.y, (face.r - eyeballR) * 2);
+    leftEye.draw(p, leftCenter.x, leftCenter.y);
+    rightEye.draw(p, rightCenter.x, rightCenter.y);
   };
 };
 
