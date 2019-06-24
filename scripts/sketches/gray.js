e3cbb669fa65c297983f27d1f78255ab9787234b
Jahyun Oh <soharu1024@gmail.com>
2019-06-24 23:54:00 +0900

    Test more gray effects

--- scripts/sketches/gray.js
+++ scripts/sketches/gray.js
@@ -1,25 +1,82 @@
-let img;
+let originalImage;
+let newImage;
+
+function rgba(image, x, y) {
+  return {
+    r: image.pixels[(y * image.width + x) * 4],
+    g: image.pixels[(y * image.width + x) * 4 + 1],
+    b: image.pixels[(y * image.width + x) * 4 + 2],
+    a: image.pixels[(y * image.width + x) * 4 + 3],
+  }
+}
+
+function writeImage(srcImage, destImage, destOffset, generateColor) {
+  for (let y = 0, offsetY = destOffset; y < srcImage.height; y += 1, offsetY += destImage.width) {
+    for (let x = 0; x < srcImage.width; x += 1) {
+      const color = generateColor(srcImage, x, y);
+      destImage.pixels[(offsetY + x) * 4] = color.r;
+      destImage.pixels[(offsetY + x) * 4 + 1] = color.g;
+      destImage.pixels[(offsetY + x) * 4 + 2] = color.b;
+      destImage.pixels[(offsetY + x) * 4 + 3] = color.a;
+    }
+  }
+}
 
 function preload() {
-  img = loadImage('https://avatars0.githubusercontent.com/u/1327853?s=460&v=4');
+  originalImage = loadImage('https://avatars0.githubusercontent.com/u/1327853?s=460&v=4');
 }
 
 function setup() {
-  createCanvas(img.width, img.height);
+  newImage = createImage(originalImage.width * 2, originalImage.height * 2);
+  createCanvas(newImage.width, newImage.height);
 
-  img.loadPixels();
-  const count = width * height * 4;
-  for (let i = 0; i < count; i += 4) {
-    const value = (img.pixels[i] + img.pixels[i + 1] + img.pixels[i + 2]) / 3;
-    img.pixels[i] = value;
-    img.pixels[i + 1] = value;
-    img.pixels[i + 2] = value;
-  }
-  img.updatePixels();
+  originalImage.loadPixels();
+  newImage.loadPixels();
+
+  writeImage(originalImage, newImage, 0, (image, x, y) => {
+    return rgba(image, x, y);
+  });
+
+  writeImage(originalImage, newImage, originalImage.width, (image, x, y) => {
+    const values = rgba(image, x, y);
+    const avg = (values.r + values.g + values.b) / 3;
+    return {
+      r: avg,
+      g: avg,
+      b: avg,
+      a: values.a,
+    }
+  });
+
+  writeImage(originalImage, newImage, newImage.width * originalImage.height, (image, x, y) => {
+    const values = rgba(image, x, y);
+    const avg = (values.r + values.g + values.b) / 3;
+    const inverted = 255 - avg;
+    return {
+      r: inverted,
+      g: inverted,
+      b: inverted,
+      a: values.a,
+    }
+  });
+
+  writeImage(originalImage, newImage, (newImage.width * originalImage.height) + originalImage.width, (image, x, y) => {
+    const values = rgba(image, x, y);
+    const avg = (values.r + values.g + values.b) / 3;
+    const bw = ( avg > 128) ? 255 : 0;
+    return {
+      r: bw,
+      g: bw,
+      b: bw,
+      a: values.a,
+    }
+  });
+
+  newImage.updatePixels();
   noLoop();
 }
 
 function draw() {
   background('#000');
-  image(img, 0, 0);
+  image(newImage, 0, 0);
 }
